9e17299ee80840625eb655048700a469922d0491
steve-kim-96 <soungku@hotmail.com>
2021-08-20 19:21:56 +1200

    initial commit

---
+++ StarterTodo/Model/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarterTodo.Model
+{
+    public class Todo
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ StarterTodo/Model/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarterTodo.Model
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+        }
+    }
+}
