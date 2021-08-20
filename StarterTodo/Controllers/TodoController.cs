fa4264fce01b0ab480965ae5596ad85a493a2d69
steve-kim-96 <soungku@hotmail.com>
2021-08-20 19:48:06 +1200

    create intial controller file

--- StarterTodo/Controllers/TodoController.cs
+++ StarterTodo/Controllers/TodoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StarterTodo.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,81 +8,20 @@ using System.Threading.Tasks;
 
 namespace StarterTodo.Controllers
 {
+    [ApiController]
     public class TodoController : Controller
+
     {
-        // GET: TodoController
+        private readonly TodoContext _context;
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+        }
+        // api/todo
+        [HttpGet("api/[controller]")]
         public ActionResult Index()
         {
             return View();
-        }
-
-        // GET: TodoController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: TodoController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: TodoController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: TodoController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: TodoController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: TodoController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: TodoController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
